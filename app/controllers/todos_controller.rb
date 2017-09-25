8b7e1ff28cfe7f2c31fa2dfb186248e5fe77dc93
Preeti Viswanathan <vis.preeti@gmail.com>
2017-09-25 12:50:16 -0700

    implement edit, update, show, and destroy actions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update]
   def index
     @todos = Todo.all
   end
@@ -8,12 +9,43 @@ class TodosController < ApplicationController
   end
 
   def create
-    @new_todo = Todo.create(params.require(:todo).permit(:title, :description, :due_date))
+    @new_todo = Todo.create(todo_params)
 
     if @new_todo.save
       redirect_to todos_path
     else
       render :new
     end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todo_path(@todo)
+    else
+      render :edit
+    end
+  end
+
+  def show
+
+  end
+
+  def destroy
+    Todo.destroy(params[:id])
+    redirect_to todos_path
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :description, :due_date)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
   end
 end
