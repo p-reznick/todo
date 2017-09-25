4a9e17cbed2b473c8f8813cadbc7f86b974ef704
Peter Reznick <preznick@gmail.com>
2017-09-25 13:59:53 -0400

    wip

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,18 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @new_todo = Todo.create(params.require(:todo).permit(:title, :description, :due_date))
+
+    if @new_todo.save
+      redirect_to todos_path
+    else
+      render :new
+    end
+  end
 end
